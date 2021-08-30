db93298084c70118f0fef616230a38875ce5a3a3
taka0614 <88304537+taka0614@users.noreply.github.com>
2021-08-30 16:22:24 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,51 @@
+
 $(function() {
-    $('.sp_menu').click(function() {
-        $('.menu_open').toggleClass('active');
-        $('.menu_text').toggleClass('active');
+    $('.menu').click(function() {
         $('.menu').toggleClass('active');
+        $('.menu-open').toggleClass('active');
+        $('.menu-open-button').toggleClass('active');
+        $('.menu_text').toggleClass('active');
         $('.menu_logo').toggleClass('active');
+        $('.menu-item').toggleClass('active');
     });
 });
 
 $(function() {
-    $('.menu-open').click(function() {
-        $('.menu_text').toggleClass('active');
+    $('.sp_menu').click(function() {
         $('.menu').toggleClass('active');
+        $('.menu-open').toggleClass('active');
+        $('.menu-open-button').toggleClass('active');
+        $('.menu_text').toggleClass('active');
         $('.menu_logo').toggleClass('active');
+        $('.menu-item').toggleClass('active');
     });
+});
+
+$(function() {
+    $('.sp_menu.sp_menu_title').click(function() {
+        $('.menu').toggleClass('active');
+        $('.menu-open').toggleClass('active');
+        $('.menu-open-button').toggleClass('active');
+        $('.menu_text').toggleClass('active');
+        $('.menu_logo').toggleClass('active');
+        $('.menu-item').toggleClass('active');
+    });
+});
+
+$(document).ready(function() {
+    $('.tab-panels .tabs li').on('click', function(){ 
+      $('.tab-panels .tabs .active').removeClass('active'); 
+      $(this).addClass('active');
+      var paneltoshow = $(this).attr('rel');
+                  
+    $('.tab-panels .panel.active').slideUp('100', function(){
+      $(this).removeClass('active');              
+
+    $('#'+paneltoshow).delay('100').slideDown(function () {
+      $(this).addClass('active');          
+    });
+   });
+
+    });
+
 });
