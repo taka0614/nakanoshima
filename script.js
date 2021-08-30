489652af41b0a7cb088fb14d9479a4f56f39bf0b
taka0614 <88304537+taka0614@users.noreply.github.com>
2021-08-30 11:56:17 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 $(function() {
-    $('.menu-open').click(function() {
+    $('.sp_menu').click(function() {
+        $('.menu_open').toggleClass('active');
         $('.menu_text').toggleClass('active');
         $('.menu').toggleClass('active');
         $('.menu_logo').toggleClass('active');
     });
 });
 
+$(function() {
+    $('.menu-open').click(function() {
+        $('.menu_text').toggleClass('active');
+        $('.menu').toggleClass('active');
+        $('.menu_logo').toggleClass('active');
+    });
+});
